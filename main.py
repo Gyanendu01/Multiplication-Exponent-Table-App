aff2e8cbfe11b5f849f03ed1496bc062cdb3d320
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-29 09:47:21 +0530

    Add features and format code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 print("\n\tWelcome to the Multiplication/Exponent Table App")
 
 def usr_input():
-    usr_name = input("\n\tEnter your username: ")
-    usr_num = float(input(("\tWhat number would you like to work with: ")))
+    try: 
+        usr_name = input("\n\tEnter your username: ")
+        usr_num = float(input(("\tWhat number would you like to work with: ")))
+    except Exception:
+        print("\n\tInvalid input!!")
     return usr_name, usr_num
 
 def usr_output(name,num):
@@ -19,3 +22,12 @@ def usr_output(name,num):
 # Main function
 usr_data = usr_input()
 usr_output(usr_data[0],usr_data[1])
+
+while True:
+    condn = input("\n\tDo you want to continue (y/n): ")
+    if condn.lower() == 'y':
+        usr_data = usr_input()
+        usr_output(usr_data[0],usr_data[1])
+    else:
+        print("\n\tTHANK YOU!!")
+        break    
